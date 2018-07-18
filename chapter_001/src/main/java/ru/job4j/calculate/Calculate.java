98990f9ecc4d49cc5aa862f19a9aae7c65ba0821
Jerk47 <romtswork@gmail.com>
2018-07-18 23:35:57 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,16 +3,25 @@ package ru.job4j.calculate;
   * Calculate.
   *
   * @author Roman Tsenov 
+  * @version $Id$
+  * @since 0.1
   */
-public class Calculate{
-	
+public class Calculate{	
 	/**
 	*Main.
 	*Output string in console.
 	*@param args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	
+	/**
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
 }
