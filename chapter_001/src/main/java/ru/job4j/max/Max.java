3cc7663a98b08fbd95c4b1558e41aaa4639298f1
Jerk47 <romtswork@gmail.com>
2018-07-25 23:47:44 +0300

    4.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,9 @@
+package ru.job4j.max;
+
+public class Max {
+
+    public int max(int first, int second) {
+        int maxNumber = first > second ? first : second;
+        return maxNumber;
+    }
+}
