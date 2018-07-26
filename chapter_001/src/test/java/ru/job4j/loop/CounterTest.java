0feca572d13c226cfbdaabc86cd5fa1052e096c0
Jerk47 <romtswork@gmail.com>
2018-07-27 00:38:41 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CounterTest {
+
+    @Test
+    public void WhenFirstNumberOneSecondNumberTenThenSum() {
+        Counter cout = new Counter();
+        int sum = cout.add(1,10);
+        assertThat(sum,is(30));
+    }
+}
