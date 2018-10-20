adb5a2639bc63fb84597e3d2b79f356503184c19
Jerk47 <romtswork@gmail.com>
2018-10-20 18:11:31 +0300

    Added pom.xml

--- chapter_002/src/main/java/Tracker/Tracker.java
+++ chapter_002/src/main/java/Tracker/Tracker.java
@@ -6,7 +6,6 @@ import java.util.Random;
 public class Tracker {
     private final Item[] items = new Item[100];
     private int position = 0;
-    Item item;
     final Random random = new Random();
 
     public Item[] getItems() {
@@ -20,7 +19,7 @@ public class Tracker {
         return item;
     }
 
-    public void replace(String id, Item item) {
+    public boolean replace(String id, Item item) {
         for (int i = 0; i <= position; i++) {
             if (this.items[i].getId().equals(id)) {
                 this.items[i] = item;
@@ -30,37 +29,36 @@ public class Tracker {
         }
     }
 
-    public void delete(String id) {
-        for (int j = 0; j <= position; j++) {
+    public boolean delete(String id) {
+        for (int j = 0; j < position; j++) {
             if (this.items[j].getId().equals(id)) {
                 System.arraycopy(items, j + 1, items, j, items.length - 1 - j);
+                position--;
                 break;
             }
         }
     }
 
     public Item[] findAll() {
-        int count = 0;
         return Arrays.copyOf(this.items, this.position);
     }
 
     public Item[] findByName(String key) {
         Item[] result = new Item[100];
         int count = 0;
-        for (int i = 0; i <= position; i++) {
-            if (this.items[i] != null) {
-                if (this.items[i].getName().equals(key)) {
-                    result[count] = this.items[i];
+        for (int i = 0; i < position; i++) {
+            if (this.items[i] != null
+                && this.items[i].getName().equals(key)) {
+                    result[count++] = this.items[i];
                 }
-                count++;
             }
-        }
         Item[] resultArr = Arrays.copyOf(result, count - 1);
         return resultArr;
     }
 
     public Item findById(String id) {
-        for (int i = 0; i <= position; i++) {
+        Item item = null;
+        for (int i = 0; i < position; i++) {
             if (this.items[i].getId().equals(id)) {
                 item = this.items[i];
                 break;
