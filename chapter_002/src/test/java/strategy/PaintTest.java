e989fa3d78ac9dee90c3882fc328f5522c62ad6e
Jerk47 <romtswork@gmail.com>
2018-12-06 21:46:23 +0300

    Added @Before and @After annotation. Refactoring.

--- chapter_002/src/test/java/strategy/PaintTest.java
+++ chapter_002/src/test/java/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -9,11 +11,21 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    PrintStream stdout = System.out;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(stdout);
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                         .append("+ + + +\n")
@@ -24,7 +36,7 @@ public class PaintTest {
                         .toString()
                 )
         );
-        System.setOut(stdout);
+
     }
 
     @Test
