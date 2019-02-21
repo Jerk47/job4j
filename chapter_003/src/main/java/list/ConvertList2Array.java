af9f1c99b929b5b7fee33b53996b309a75b8b815
Rocket47 <romtswork@gmail.com>
2019-02-21 23:48:16 +0300

    Initial version of ConvertList2Array program. Need to add a general logic.

---
+++ chapter_003/src/main/java/list/ConvertList2Array.java
@@ -0,0 +1,12 @@
+package list;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public int[][] toArray(List<Integer> list, int rows) {
+        int cells = 0;
+        int[][] array = new int[rows][cells];
+        return array;
+    }
+}
+
