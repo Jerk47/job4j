7e349a20a43a2d298f6ad3c6fe0e71ac2846f4bd
Rocket47 <romtswork@gmail.com>
2019-04-01 23:56:47 +0300

    Initial condition of the algorithm.

--- chapter_003/src/main/java/list/ConvertList2Array.java
+++ chapter_003/src/main/java/list/ConvertList2Array.java
@@ -1,12 +1,23 @@
 package list;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
         int cells = 0;
         int[][] array = new int[rows][cells];
+        for (int i: list) {
+            while (i < rows) {
+                i++;
+            }
+
+        }
+
+            
+
         return array;
     }
 }
+
 
