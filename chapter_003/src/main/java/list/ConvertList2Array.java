c9512e512212654e4ed354827608964c956dc4a2
Rocket47 <romtswork@gmail.com>
2019-04-02 23:54:46 +0300

    Current version of the task convert ArrayList to 2d array

--- chapter_003/src/main/java/list/ConvertList2Array.java
+++ chapter_003/src/main/java/list/ConvertList2Array.java
@@ -5,18 +5,27 @@ import java.util.List;
 
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
-        int cells = 0;
+        int cells = list.size();
         int[][] array = new int[rows][cells];
-        for (int i: list) {
-            while (i < rows) {
-                i++;
+        for (int i = 0; i < rows; i++) {
+            for (int j : list) {
+                array[i][cells] = j;
             }
 
         }
-
-            
-
         return array;
+    }
+
+    public static void main(String[] args) {
+        ConvertList2Array cl = new ConvertList2Array();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(cl.toArray(list, 2));
     }
 }
 
