6b11b7d2854e455094634f68d927dba3a7113b87
Rocket47 <romtswork@gmail.com>
2019-04-26 01:42:03 +0300

    Created function of ConvertList2Array

--- chapter_003/src/main/java/list/ConvertList2Array.java
+++ chapter_003/src/main/java/list/ConvertList2Array.java
@@ -1,31 +1,32 @@
 package list;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
-        int cells = list.size();
+        int cells = rows;
         int[][] array = new int[rows][cells];
-        for (int i = 0; i < rows; i++) {
-            for (int j : list) {
-                array[i][cells] = j;
+        int cellsCount = 0;
+        int rowsCount = 0;
+        int listSize = list.size();
+        if (listSize % rows != 0) {
+            for (int j = 0; j < listSize / rows; j++) {
+                list.add(0);
             }
-
+        }
+        for (Integer i : list
+        ) {
+            if (cellsCount < rows) {
+                array[rowsCount][cellsCount] = i;
+                cellsCount++;
+            } else if (cellsCount == rows) {
+                rowsCount++;
+                cellsCount = 0;
+                array[rowsCount][cellsCount] = i;
+                cellsCount++;
+            }
         }
         return array;
-    }
-
-    public static void main(String[] args) {
-        ConvertList2Array cl = new ConvertList2Array();
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        System.out.println(cl.toArray(list, 2));
     }
 }
 
