004346f52a94894cb29793fc18d9add19d78882b
Rocket47 <romtswork@gmail.com>
2019-04-29 23:40:24 +0300

    Created method convert() which converts list with some arrays to List<Integer>

--- chapter_003/src/test/java/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/list/ConvertList2ArrayTest.java
@@ -2,7 +2,9 @@ package list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -21,5 +23,24 @@ public class ConvertList2ArrayTest {
                 {7, 0, 0}
         };
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whneAddTwoArrays() {
+        ConvertList2Array cl = new ConvertList2Array();
+        List<int[]> list = new ArrayList<>();
+        List<Integer> finalList = new ArrayList<>();
+        List<Integer> compareList = new ArrayList<>();
+        compareList.add(1);
+        compareList.add(2);
+        compareList.add(3);
+        compareList.add(4);
+        compareList.add(5);
+        compareList.add(6);
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6});
+        finalList = cl.convert(list);
+        assertThat(finalList, is(compareList));
+
     }
 }
