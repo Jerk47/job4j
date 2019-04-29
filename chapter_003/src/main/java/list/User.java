a2c9b69e1e3271fd83886867db397c007aa8ab9e
Rocket47 <romtswork@gmail.com>
2019-04-30 00:57:42 +0300

    Created class User. Created method UserConvert. This method converts List<User> to HashMap<Integer, User>

---
+++ chapter_003/src/main/java/list/User.java
@@ -0,0 +1,14 @@
+package list;
+
+public class User {
+    int id;
+    String name;
+    String city;
+
+    public User(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+}
+
