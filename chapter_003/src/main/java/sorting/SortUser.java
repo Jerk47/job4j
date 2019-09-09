13e6f5d21263dc5d4ea1185e62862b8bb169a2d8
Rocket47 <roman.tsenov@gmail.com>
2019-09-09 19:16:41 +0300

    Added logic for comparator

--- chapter_003/src/main/java/sorting/SortUser.java
+++ chapter_003/src/main/java/sorting/SortUser.java
@@ -3,7 +3,7 @@ package sorting;
 import java.util.*;
 
 public class SortUser {
-    static class User implements Comparable<User> {
+    static class User implements  Comparable<User>{
         private final String age;
         private final String name;
 
@@ -17,15 +17,57 @@ public class SortUser {
             return this.age.compareTo(o.age);
         }
 
+
         @Override
         public String toString() {
-            return age;
+            return name + " " + age;
         }
+
     }
 
     public Set<User> sort(List<User> users) {
         Set<User> sorted_users = new TreeSet<>();
         sorted_users.addAll(users);
         return sorted_users;
+    }
+
+    public List<User> sortNameLength(List<User> users) {
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                return user1.name.length() > user2.name.length() ? 1 : user1.name.length() < user2.name.length() ? -1 : 0;
+            }
+        });
+        return users;
+    }
+
+    public List<User> sortByAllField(List<User> users) {
+        Collections.sort(users, new Comparator<User>() {
+            @Override
+            public int compare(User user1, User user2) {
+                int name = user1.name.compareTo(user2.name);
+                if (name == 0) {
+                    return name;
+                }
+                return user1.age.length() > user2.age.length() ? 1 : user1.age.length() < user2.age.length() ? 1 : 0;
+            }
+        });
+        return users;
+    }
+
+    public static void main(String[] args) {
+
+        SortUser su = new SortUser();
+        List<User> users = new ArrayList<User>();
+        List<User> resultLIst = new ArrayList<>();
+        users.add(new User("Sergey", "25"));
+        users.add(new User("Ivan", "30"));
+        users.add(new User("Sergey", "20"));
+        users.add(new User("Ivan", "25"));
+        resultLIst = su.sortNameLength(users);
+        for (User i : resultLIst) {
+            System.out.println(i);
+        }
+
     }
 }
