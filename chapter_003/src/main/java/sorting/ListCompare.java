86d3f978e4815eb2e078f44dd2e7fc29e808cc45
Rocket47 <romtswork@gmail.com>
2019-11-22 18:49:57 +0300

    Fixed loop counter and logic to get result.

--- chapter_003/src/main/java/sorting/ListCompare.java
+++ chapter_003/src/main/java/sorting/ListCompare.java
@@ -6,10 +6,9 @@ public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = 0;
-        boolean checkEquality;
-        checkEquality = true;
+        boolean checkEquality = true;
         int minLength = Math.min(left.length(), right.length());
-        for (int i = 0; i < minLength - 1; i++) {
+        for (int i = 0; i < minLength; i++) {
             result = Character.compare(left.charAt(i), right.charAt(i));
             if (result != 0) {
                 checkEquality = false;
@@ -17,10 +16,7 @@ public class ListCompare implements Comparator<String> {
             }
         }
         if (checkEquality) {
-            result = minLength == left.length() ? -1 : 1;
-            if (left.length() == right.length()) {
-                result = 0;
-            }
+            result = left.length() - right.length();
         }
         return result;
     }
