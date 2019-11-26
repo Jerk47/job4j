dab32ef0fa6383cc214739fbb6db3fe1269ea8c7
Rocket47 <romtswork@gmail.com>
2019-11-26 17:42:36 +0300

    Created first architecture for bank

---
+++ chapter_003/src/main/java/bank/Account.java
@@ -0,0 +1,29 @@
+package bank;
+
+public class Account {
+    private double value;
+    private int requisites;
+
+    public Account(double value, int requisites) {
+        this.value = value;
+        this.requisites = requisites;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public int getRequisites() {
+        return requisites;
+    }
+
+    public void setRequisites(int requisites) {
+        this.requisites = requisites;
+    }
+
+
+}
---
+++ chapter_003/src/main/java/bank/Bank.java
@@ -0,0 +1,36 @@
+package bank;
+
+import java.util.List;
+
+public class Bank {
+
+    public void addUser(User user) {
+
+    }
+
+    public void deleteUser(User user) {
+
+    }
+
+    public void addAccountFromUser(String passport, Account account) {
+
+    }
+
+
+    public void deleteAccountFromUser(String passport, Account account) {
+
+    }
+
+    public List<Account> getUserAccounts(String passport) {
+
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite, String destPassport, String dstRequisite, double amount) {
+
+    }
+
+
+}
+
+
