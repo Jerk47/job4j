04ffa04b0adc81738a6a74b9edf093b5b3ffb7d2
Rocket47 <romtswork@gmail.com>
2019-12-07 14:41:59 +0300

    Commit after checstyle

--- chapter_003/src/test/java/function/FunctionDiapasonTest.java
+++ chapter_003/src/test/java/function/FunctionDiapasonTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -18,14 +19,14 @@ public class FunctionDiapasonTest {
 
     @Test
     public void whenQuadraticFunctionThenQuadraticResults() {
-        List<Double> result = new FunctionDiapason().diapason(5, 8, x -> Math.pow(x, 2)) ;
+        List<Double> result = new FunctionDiapason().diapason(5, 8, x -> Math.pow(x, 2));
         List<Double> expected = Arrays.asList(25D, 36D, 49D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenLogarithmicFunctionThenLogarithmicResults() {
-        List<Double> result = new FunctionDiapason().diapason(5, 8, StrictMath::log) ;
+        List<Double> result = new FunctionDiapason().diapason(5, 8, StrictMath::log);
         List<Double> expected = Arrays.asList(1.6094379124341003, 1.791759469228055, 1.9459101490553132);
         assertThat(result, is(expected));
     }
